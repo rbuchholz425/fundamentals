8a9fa8c8642576014f717c0c038ab55cd4acc68e
Ryan Buchholz <rbuchholz425@gmail.com>
2020-01-02 22:03:36 -0700

    Prework on various fundamentals

---
+++ variables-and-data.js
@@ -0,0 +1,16 @@
+/*
+* Variables, Data Types, and Typing
+ */
+let add = 1 + 2;
+let sub = 2 -1;
+let multiplication= 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string);
+
+/*
+*Wasn't sure if I what to leave from this portion of the screencast, so I just followed along and left the last one shown.
+ */
